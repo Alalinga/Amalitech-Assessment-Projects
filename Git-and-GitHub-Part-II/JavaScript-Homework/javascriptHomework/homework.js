0afe63343a9e82989b08d3704b138e6c07e3b41e
Alalinga <alalinga.mubarik@amalitech.org>
2021-12-15 03:31:09 +0000

    adding Git-and-GitHub-Part-II project

---
+++ Git-and-GitHub-Part-II/JavaScript-Homework/javascriptHomework/homework.js
@@ -0,0 +1,33 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+var APPLE_QUANTITY = 5;
+
+if (APPLE_QUANTITY < 5){
+  //printing on console
+  console.log("You have fewer than 5 apples.");
+} else {
+  //printing on console
+  console.log("You have 5 or more apples. Hurrah!");
+}
+
+// 2. Write a "for" loop
+for(i = 0; i < APPLE_QUANTITY; i ++){
+  //printing on console
+  console.log("WEEEEEEE! I'm looping!")
+}
+
+// 3. Write a function
+var sayHello = function(){
+  //printing on console
+  console.log("Hello!")
+}
+
+// 4. Create an object
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK"
+}
+
+
